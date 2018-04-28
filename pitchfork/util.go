7312542ebef58dc51f85e3d79d01f6cce4adc8be
naoto0822 <n.h.in.m.h@gmail.com>
2018-04-28 21:00:49 +0900

    add comment out

--- pitchfork/util.go
+++ pitchfork/util.go
@@ -5,8 +5,11 @@ import (
 )
 
 // RSS2 date format example: Sat, 28 Apr 2018 05:18:00 +0000
-// see: https://golang.org/pkg/time/#pkg-constants
-//   - RFC1123Z = "Mon, 02 Jan 2006 15:04:05 -0700"
+// following doc is written "the Date and Time Specification of RFC 822", but actually it is RFC 1123 format.
+// cf. https://cyber.harvard.edu/rss/rss.html
+//
+// RFC1123Z = "Mon, 02 Jan 2006 15:04:05 -0700"
+// cf. https://golang.org/pkg/time/#pkg-constants
 func parseTime(s string) (time.Time, error) {
 	return time.Parse(time.RFC1123Z, s)
 }
