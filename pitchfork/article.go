2dd2bcd3854640aa3c3710d10ee106d5c025a9fa
naoto0822 <n.h.in.m.h@gmail.com>
2018-05-03 01:24:54 +0900

    fix comment out

--- pitchfork/article.go
+++ pitchfork/article.go
@@ -10,7 +10,7 @@ Example: News Feed
 
 <item>
   <title>Vampire Weekend Return at Surprise Show</title>
-  <description>For HQ Trivia host Scott Rogowsky’s “Running Late” event in L.A.</description>
+  <description>For HQ Trivia host Scott Rogowsky’s Running Late event in L.A.</description>
   <link>https://pitchfork.com/news/vampire-weekend-return-at-surprise-show</link>
   <guid isPermaLink="false">5ae0e28ca85afb79a8445d2e</guid>
   <pubDate>Wed, 25 Apr 2018 21:15:34 +0000</pubDate>
@@ -18,7 +18,7 @@ Example: News Feed
   <category>News</category>
   <dc:creator>Sam Sodomsky</dc:creator>
   <dc:modified>Thu, 26 Apr 2018 14:53:59 +0000</dc:modified>
-  <dc:publisher>Condﾃｩ Nast</dc:publisher>
+  <dc:publisher>Cond Nast</dc:publisher>
   <media:thumbnail url="https://media.pitchfork.com/photos/5ae0e6c08bec5b23c213a369/master/pass/Vampire_Weekend.jpg" width="790" height="395"/>
 </item>
 
