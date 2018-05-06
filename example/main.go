501a860ec3855f782ff83fd91a69f9e0ebcf5a5f
naoto0822 <n.h.in.m.h@gmail.com>
2018-05-07 01:11:51 +0900

    sample fetching news feed

--- example/main.go
+++ example/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/naoto0822/go-pitchfork/pitchfork"
+)
 
 func main() {
 	fmt.Println("go-pitchfork!")
+
+	// News Feed
+	c := pitchfork.NewClient()
+	articles, err := c.News.Fetch()
+	if err != nil {
+		fmt.Errorf("failed fetch News Feed err:", err)
+	}
+	for _, a := range articles {
+		fmt.Printf("%+v", a)
+	}
 }
