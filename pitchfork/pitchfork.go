a5b6e3dd4892ee30d3fa652e1e14ae06f4b5088e
naoto0822 <n.h.in.m.h@gmail.com>
2018-05-07 01:09:35 +0900

    modify pitchfork.NewClient

--- pitchfork/pitchfork.go
+++ pitchfork/pitchfork.go
@@ -4,18 +4,41 @@ import ()
 
 // Client is communication PitchFork RSS
 type Client struct {
-	baseURL string
-	rss     *gss.Client
+	News            *NewsService
+	AlbumReviews    *AlbumReviewsService
+	TrackReviews    *TrackReviewsService
+	Pitch           *PitchService
+	Features        *FeaturesService
+	BestNewAlbums   *BestNewAlbumsService
+	BestNewReissues *BestNewReissuesService
+	BestNewTracks   *BestNewTracksService
 
-	// TODO: later
-	userAgent string
+	// TODO: later implements and attachable HTTP header
+	// userAgent string
 }
 
 // NewClient factory pitchfork.Client
 func NewClient() *Client {
-	c := Client{}
+	repo := newArticleRepository()
+	urlBuilder := newURLBuilder()
 
-	// TODO:
+	news := NewNewsService(repo, urlBuilder)
+	albumReviews := NewAlbumReviewsService(repo, urlBuilder)
+	trackReviews := NewTrackReviewsService(repo, urlBuilder)
+	pitch := NewPitchService(repo, urlBuilder)
+	features := NewFeaturesService(repo, urlBuilder)
+	bestNewAlbums := NewBestNewAlbumsService(repo, urlBuilder)
+	bestNewReissues := NewBestNewReissuesService(repo, urlBuilder)
+	bestNewTracks := NewBestNewTracksService(repo, urlBuilder)
 
-	return &c
+	return &Client{
+		News:            NewsService,
+		AlbumReviews:    albumReviews,
+		TrackReviews:    trackReviews,
+		Pitch:           pitch,
+		Features:        features,
+		BestNewAlbums:   bestNewAlbums,
+		BestNewReissues: bestNewReissues,
+		BestNewTracks:   bestNewTracks,
+	}
 }
