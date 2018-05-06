184e9632559abd7368b6c71d04fda41f52498c17
naoto0822 <n.h.in.m.h@gmail.com>
2018-05-06 20:23:50 +0900

    add feature service

---
+++ pitchfork/features_service.go
@@ -0,0 +1,31 @@
+package pitchfork
+
+// FeaturesService
+// cf. https://pitchfork.com/rss/features/
+type FeaturesService struct {
+	articleRepository articleRepository
+	urlBuilder        urlBuilder
+}
+
+// NewFeaturesService factory FeaturesService
+func NewFeaturesService(a articleRepository, b *urlBuilder) *FeaturesService {
+	return &FeaturesService{
+		articleRepository: a,
+		urlBuilder:        u,
+	}
+}
+
+// Fetch Features Feed
+func (s *FeaturesService) Fetch() ([]Article, error) {
+	url, err := s.urlBuilder.build(newsType)
+	if err != nil {
+		return nil, err
+	}
+
+	articles, err := s.articleRepository.fetch(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return articles, nil
+}
