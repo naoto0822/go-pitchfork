73c30845cbb1b091f790ac6bcef59d4c655d0d99
naoto0822 <n.h.in.m.h@gmail.com>
2018-05-06 20:52:32 +0900

    add Best New Albums service

---
+++ pitchfork/best_new_albums_service.go
@@ -0,0 +1,30 @@
+package pitchfork
+
+// BestNewAlbumsService cf. https://pitchfork.com/rss/reviews/best/albums/
+type BestNewAlbumsService struct {
+	articleRepository articleRepository
+	urlBuilder        *urlBuilder
+}
+
+// NewBestNewAlbumsService factory BestNewAlbumsService
+func NewBestNewAlbumsService(a articleRepository, u urlBuilder) *BestNewAlbumsService {
+	return &BestNewAlbumsService{
+		articleRepository: a,
+		urlBuilder:        u,
+	}
+}
+
+// Fetch Best New Albums Feed
+func (s *BestNewAlbumsService) Fetch() ([]Article, error) {
+	url, err := s.urlBuilder.build(newsType)
+	if err != nil {
+		return nil, err
+	}
+
+	articles, err := s.articleRepository.fetch(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return articles, nil
+}
