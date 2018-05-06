1f231c383bfca2e99d1f80a15f0b84a171334dd6
naoto0822 <n.h.in.m.h@gmail.com>
2018-05-06 21:46:20 +0900

    add best new resissues service

---
+++ pitchfork/best_new_reissues_service.go
@@ -0,0 +1,30 @@
+package pitchfork
+
+// BestNewReissuesService cf. https://pitchfork.com/rss/reviews/best/reissues/
+type BestNewReissuesService struct {
+	articleRepository articleRepository
+	urlBuilder        *urlBuilder
+}
+
+// NewBestNewReissuesService factory BestNewReissuesService
+func NewBestNewReissuesService(a articleRepository, u urlBuilder) *BestNewReissuesService {
+	return &BestNewReissuesService{
+		articleRepository: a,
+		urlBuilder:        u,
+	}
+}
+
+// Fetch Best New Reissues Feed
+func (s *BestNewReissuesService) Fetch() ([]Article, error) {
+	url, err := s.urlBuilder.build(newsType)
+	if err != nil {
+		return nil, err
+	}
+
+	articles, err := s.articleRepository.fetch(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return articles, nil
+}
