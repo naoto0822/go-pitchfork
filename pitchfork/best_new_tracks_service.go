9aaba3ec6821480f34df2d4fa561cc6054a7da7b
naoto0822 <n.h.in.m.h@gmail.com>
2018-05-06 22:03:47 +0900

    add best new tracks service

---
+++ pitchfork/best_new_tracks_service.go
@@ -0,0 +1,30 @@
+package pitchfork
+
+// BestNewTracksService cf. https://pitchfork.com/rss/reviews/best/tracks/
+type BestNewTracksService struct {
+	articleRepository articleRepository
+	urlBuilder        urlBuilder
+}
+
+// NewBestNewTracksService factory BestNewTracksService
+func NewBestNewTracksService(a articleRepository, u urlBuilder) *NewBestNewTracksService {
+	return &BestNewTracksService{
+		articleRepository: a,
+		urlBuilder:        u,
+	}
+}
+
+// Fetch Best New Tracks Feed
+func (s *BestNewTracksService) Fetch() ([]Article, error) {
+	url, err := s.urlBuilder.build(newsType)
+	if err != nil {
+		return nil, err
+	}
+
+	articles, err := s.articleRepository.fetch(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return articles, nil
+}
